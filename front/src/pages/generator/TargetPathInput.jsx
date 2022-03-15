8f552ba89edd81c918399c8e407405ceb50309c8
zkboys <876213774@qq.com>
2022-03-15 13:33:43 +0800

    :ambulance: 子模板未获取到字段问题

--- front/src/pages/generator/TargetPathInput.jsx
+++ front/src/pages/generator/TargetPathInput.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import { ExclamationCircleOutlined, CheckCircleOutlined } from '@ant-design/icons';
+import React, {useEffect, useState} from 'react';
+import {ExclamationCircleOutlined, CheckCircleOutlined} from '@ant-design/icons';
 import ajax from 'src/commons/ajax';
-import { Input } from 'antd';
-import { stringFormat } from 'src/commons';
+import {Input} from 'antd';
+import {stringFormat} from 'src/commons';
 
 function TargetPathInput(props) {
     const { value, form, name, moduleNames, checkExist, templateOptions, templateId, ...others } = props;
     const [exist, setExist] = useState(false);
 
-    // 根据模块名，处理地址
+    // value 改变 根据模块名，处理地址
     useEffect(() => {
         const path = value || templateOptions.find(item => item.value === templateId)?.record?.targetPath;
         if (!path) return;
         const val = path.includes('{') ? stringFormat(path, moduleNames) : path;
         form.setFields([{ name, value: val }]);
     }, [form, name, value, moduleNames, templateId, templateOptions]);
+
+    // moduleNames 改变 根据模块名，处理地址
+    useEffect(() => {
+        const path = templateOptions.find(item => item.value === templateId)?.record?.targetPath;
+        const val = stringFormat(path, moduleNames);
+        form.setFields([{ name, value: val }]);
+    }, [form, moduleNames, name, templateId, templateOptions]);
 
     // 检测文件是否存在
     useEffect(() => {
@@ -30,7 +37,7 @@ function TargetPathInput(props) {
         <div>
             <Input value={value} {...others} />
             <span style={{ marginLeft: 4 }}>
-                {exist ? <ExclamationCircleOutlined style={{ color: 'orange' }} /> : <CheckCircleOutlined style={{ color: 'green' }} />}
+                {exist ? <ExclamationCircleOutlined style={{ color: 'orange' }}/> : <CheckCircleOutlined style={{ color: 'green' }}/>}
             </span>
         </div>
     );
