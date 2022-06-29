ece48b382798f5686389dc13b179386ec9a69ba3
zkboys <876213774@qq.com>
2022-06-29 11:23:08 +0800

    :sparkles: 去掉oracle支持

--- db/index.js
+++ db/index.js
@@ -1,12 +1,10 @@
 const MySql = require('./MySql');
-const Oracle = require('./Oracle');
 
 function getDb(url) {
     const data = new URL(url);
     const { protocol } = data;
 
     if (protocol === 'mysql:') return new MySql(url);
-    if (protocol === 'oracle:') return new Oracle(url);
 
     throw Error(`${protocol} is not supported!`);
 }
